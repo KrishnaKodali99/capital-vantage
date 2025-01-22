d148efde767f3b016655cf145c4ab6a1d731b271
Krishna Kodali <krishnakodali81@gmail.com>
2025-01-22 12:50:08 -0500

    New: Init BE repo using Spring Boot

---
+++ capital-vantage-be/src/main/java/com/capitalvantage/cvbe/CapitalVantageBeApplication.java
@@ -0,0 +1,13 @@
+package com.capitalvantage.cvbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalVantageBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalVantageBeApplication.class, args);
+	}
+
+}
---
+++ capital-vantage-be/src/test/java/com/capitalvantage/cvbe/CapitalVantageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitalvantage.cvbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalVantageBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
